a58bf7f69e302f3d40638f9fb08246a3b993f5ef
ccoenraets <ccoenraets@gmail.com>
2014-02-19 10:10:37 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+## Interactive Mobile Dashboard using ChartJS and Cordova ##
+
+To run in your browser:
+
+1. Download ChartJS from http://chartjs.devexpress.com/
+
+2. Copy dx.chartjs.js and globalize.min.js in www/lib/chartjs
+
+3. Open www/index.html in tour browser
+
+
+To run as a Cordova app:
+
+
+1. Download ChartJS from http://chartjs.devexpress.com/
+
+2. Copy dx.chartjs.js and globalize.min.js in www/lib/chartjs
+
+3. Open Terminal and type:
+
+```
+cordova create olympic-dahboard-chartjs
+cd olympic-dahboard-chartjs
+cordova platforms add ios
+cordova plugin add org.apache.cordova.device
+cordova plugin add org.apache.cordova.console
+cordova plugin add org.apache.cordova.statusbar
+```
+
+4. Delete the www folder that was created and replace it with the www folder from this repo
+
+5. In terminal type:
+
+```
+cordova build ios
+```
+
+6. Open the .xcodeproj file in the platforms/ios folder and run the app in the emulator or on your iOS device. To run the app on your iOS device, you need an Apple developer certificate and an app provisioning profile.
+
---
+++ www/js/dashboard1.js
@@ -0,0 +1,124 @@
+var dashboard1 = (function () {
+
+    "use strict";
+
+    // Currently selected values in the dashboard
+    var selectedYear = "2010",
+        selectedCountryIdx = 0,
+        selectedCountry = "USA";
+
+    /* Functions to create the individual charts involved in the dashboard */
+
+    function createLineChart(selector) {
+        $(selector).dxChart({
+            dataSource: summary,
+            animation: {
+                duration: 350
+            },
+            commonSeriesSettings: {
+                argumentField: "year"
+            },
+            series: [
+                { valueField: "Canada", name: "Canada" },
+                { valueField: "Germany", name: "Germany" },
+                { valueField: "Norway", name: "Norway" },
+                { valueField: "Russia", name: "Russia" },
+                { valueField: "USA", name: "USA" }
+            ],
+            argumentAxis: {
+                grid: {
+                    visible: true
+                }
+            },
+            tooltip: {
+                enabled: true
+            },
+            title: {
+                text: "Top 5 Medal Countries",
+                font: {
+                    size: "24px"
+                }
+            },
+            legend: {
+                verticalAlignment: "bottom",
+                horizontalAlignment: "center"
+            },
+            commonPaneSettings: {
+                border: {
+                    visible: true,
+                    right: false
+                }
+            },
+            pointClick: function (clickedPoint, clickEvent) {
+                selectedYear = clickedPoint.argument.substr(0, 4);
+                updateBarChart();
+            }
+        });
+    }
+
+    function createBarChart(selector) {
+        return $(selector).dxChart({
+            dataSource: results[selectedYear],
+            animation: {
+                duration: 350
+            },
+            title: {
+                text: "Total Medals by Country in " + selectedYear,
+                font: {
+                    size: "24px"
+                }
+            },
+            legend: {
+                visible: false
+            },
+            series: {
+                argumentField: "Country",
+                valueField: "Total",
+                name: "Medals",
+                type: "bar",
+                color: '#ffa500'
+            },
+            pointClick: function (clickedPoint, clickEvent) {
+                selectedCountry = clickedPoint.argument;
+                for (var i = 0; i < results[selectedYear].length; i++) {
+                    if (results[selectedYear][i]["Country"] === selectedCountry) {
+                        selectedCountryIdx = i;
+                        break;
+                    }
+                }
+                updatePieChart();
+                updateHBarChart();
+            }
+        });
+    }
+
+    /* Functions to update individual charts when their underlying dataset changes */
+
+    function updateBarChart() {
+        var chart2 = $("#chart2").dxChart("instance");
+        chart2.option({
+            dataSource: results[selectedYear],
+            title: "Total Medals by Country in " + selectedYear
+        });
+    }
+
+    /* Render the dashboard */
+
+    function render() {
+
+        var html =
+            '<div id="chart1" class="chart2"></div>' +
+                '<div id="chart2" class="chart2"></div>';
+
+        $("#content").html(html);
+
+        createLineChart('#chart1');
+        createBarChart('#chart2');
+
+    }
+
+    return {
+        render: render
+    }
+
+}());
